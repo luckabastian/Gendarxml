bef041bed4f942c6d765f3b9f5b1cb158bad9947
Gendarxml <nurinah904@gmail.com>
2024-12-25 01:49:49 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -90,10 +90,10 @@ function validatePort(port) {
 
 // Generate VLESS Link
 function generateVlessLink(proxy, port) {
-  return `vless://${passuid}@${servervless}:443?encryption=none&security=tls&sni=${servervless}&fp=randomized&type=ws&host=${servervless}&path=%2Fproxy%3D${encodeURIComponent(proxy)}%26port%3D${port}#VLESS_${proxy}`;
+  return `vless://${passuid}@${servervless}:443?encryption=none&security=tls&sni=${servervless}&fp=randomized&type=ws&host=${servervless}&path=%2Fproxy%2F${proxy}%2Fport%2F${port}#VLESS_${proxy}`;
 }
 
 // Generate Trojan Link
 function generateTrojanLink(proxy, port) {
-  return `trojan://${passuid}@${servertrojan}:443?encryption=none&security=tls&sni=${servertrojan}&fp=randomized&type=ws&host=${servertrojan}&path=%2Fproxy%3D${encodeURIComponent(proxy)}%26port%3D${port}#Trojan_${proxy}`;
+  return `trojan://${passuid}@${servertrojan}:443?encryption=none&security=tls&sni=${servertrojan}&fp=randomized&type=ws&host=${servertrojan}&path=%2Fproxy%2F${proxy}%2Fport%2F${port}#Trojan_${proxy}`;
 }
